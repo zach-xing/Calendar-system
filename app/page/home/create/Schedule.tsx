62f9b339047af9464858e8d1156cba909b220c1c
Zeekg <3193373997@qq.com>
2022-10-11 23:58:01 +0800

    feat(app): add create schedule'page UI

--- app/page/home/create/Schedule.tsx
+++ app/page/home/create/Schedule.tsx
@@ -1,29 +1,129 @@
 import { View, StyleSheet } from "react-native";
 import React from "react";
-import { Button, Card, Layout, Text } from "@ui-kitten/components";
-
-const Footer = (props) => (
-  <View {...props}>
-    <Button size="small" status="basic">
-      CANCEL
-    </Button>
-    <Button size="small">ACCEPT</Button>
-  </View>
-);
+import {
+  Card,
+  Layout,
+  Text,
+  Input,
+  Toggle,
+  Button,
+  Datepicker,
+} from "@ui-kitten/components";
+import { useForm, Controller } from "react-hook-form";
 
 /**
  * 日程 comp
  */
 export default function Schedule() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [isFullDay, setIsFullDay] = React.useState(false);
+  const [date, setDate] = React.useState(new Date());
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <Layout style={styles.container}>
-      <Card footer={Footer}>
-        <Text>
-          The Maldives, officially the Republic of Maldives, is a small country
-          in South Asia, located in the Arabian Sea of the Indian Ocean. It lies
-          southwest of Sri Lanka and India, about 1,000 kilometres (620 mi) from
-          the Asian continent
-        </Text>
+      <Card appearance="filled">
+        <Controller
+          name="title"
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              value={value}
+              label="标题"
+              placeholder="Place your title"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              status={errors.title ? "danger" : "basic"}
+            />
+          )}
+        />
+
+        {/* 选择日程的时间 */}
+        <View style={styles.gap}>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              ...styles.gap,
+            }}
+          >
+            <Text>全天</Text>
+            <Toggle
+              checked={isFullDay}
+              onChange={(res) => {
+                setIsFullDay(res);
+              }}
+            />
+          </View>
+
+          {isFullDay ? (
+            // 全天
+            <Controller
+              name="dataString"
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, value } }) => (
+                <Datepicker
+                  date={value}
+                  onSelect={onChange}
+                  status={errors.dataString ? "danger" : "basic"}
+                />
+              )}
+            />
+          ) : (
+            // 不是 全天
+            <>
+              <Controller
+                name="startTime"
+                control={control}
+                rules={{
+                  required: true,
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <Input
+                    value={value}
+                    label="开始时间"
+                    placeholder="Place your start time"
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    status={errors.title ? "danger" : "basic"}
+                  />
+                )}
+              />
+              <Controller
+                name="endTime"
+                control={control}
+                rules={{
+                  required: true,
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <Input
+                    value={value}
+                    label="结束时间"
+                    placeholder="Place your end time"
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    status={errors.title ? "danger" : "basic"}
+                  />
+                )}
+              />
+            </>
+          )}
+        </View>
+
+        <Button onPress={handleSubmit(onSubmit)}>Submit</Button>
       </Card>
     </Layout>
   );
@@ -33,5 +133,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
+  },
+  gap: {
+    marginVertical: 10,
   },
 });
