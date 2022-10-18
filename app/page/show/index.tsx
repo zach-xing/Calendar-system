ef0935d40570480754e6eb5a2e814adc3e10942b
Zeekg <3193373997@qq.com>
2022-10-19 00:09:02 +0800

    feat(app): add show event UI

--- app/page/show/index.tsx
+++ app/page/show/index.tsx
@@ -1,17 +1,44 @@
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 import React from "react";
-import { useRoute } from "@react-navigation/native";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import { Icon, Layout, Button, Text } from "@ui-kitten/components";
 
 /**
  * 显示某个事件详情
  */
 export default function ShowScreen() {
   const route = useRoute();
-  console.log(route.params);
-  
+  const data: RNType.ScheduleType | RNType.ImportantDayType = route.params as any;
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>ShowScreen</Text>
-    </View>
+    <Layout style={styles.container}>
+      <View style={styles.headerStyle}>
+        <Button
+          appearance="ghost"
+          accessoryLeft={
+            <Icon name="arrow-ios-back" style={{ width: 40, height: 40 }} />
+          }
+          onPress={() => navigation.goBack()}
+        />
+      </View>
+      <View style={styles.bodyStyle}>
+        <Text category="h4">{data.title}</Text>
+      </View>
+    </Layout>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+  },
+  headerStyle: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  bodyStyle: {
+    paddingHorizontal: 20,
+  },
+});
