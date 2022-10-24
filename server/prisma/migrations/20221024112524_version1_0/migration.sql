e2bd63d15e5325657171922e140c83086eb1c241
Zeekg <3193373997@qq.com>
2022-10-24 19:26:09 +0800

    feat(server): initial MYSQL db

---
+++ server/prisma/migrations/20221024112524_version1_0/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL DEFAULT '',
+    `account` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
