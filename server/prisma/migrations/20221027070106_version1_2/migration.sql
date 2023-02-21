e891c7d64b832c96798dc7919077b839b0a8df9d
Zeekg <zeekg2001@gmail.com>
2023-02-21 23:29:46 +0800

    refactor: switched to turbo monorepo

--- server/prisma/migrations/20221027070106_version1_2/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `event` (
-    `id` VARCHAR(191) NOT NULL,
-    `list` VARCHAR(191) NOT NULL,
-
-    UNIQUE INDEX `event_id_key`(`id`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- server/src/resources/user/dto/login-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class LoginUserDto {
-  @IsString()
-  account: string;
-
-  @IsString()
-  password: string;
-}
