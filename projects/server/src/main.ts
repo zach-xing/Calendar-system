7101aa6e9edbdd65415d6973d6615cae1c9469d1
Zeekg <zeekg2001@gmail.com>
2023-02-23 22:44:59 +0800

    feat(server): add User resource

--- projects/server/src/main.ts
+++ projects/server/src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        // 开启全局隐式转换，表示我们不需要 @Type 显示转换了
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   await app.listen(8080);
 }
 bootstrap();
