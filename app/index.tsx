1935a21fa6a9b4993a17eb4823c3fcfd4ca8ed34
Zeekg <zeekg2001@gmail.com>
2023-02-26 23:38:23 +0800

    feat: add Calendar comp UI style

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { Link, Stack } from "expo-router";
 import Toast from "react-native-toast-message";
 import { Button, Icon, ListItem } from "@rneui/base";
 import { Calendar } from "react-native-calendars";
 import type { DateData } from "react-native-calendars";
-import { SpeedDial, Text } from "@rneui/themed";
+import { SpeedDial, Tab, Text } from "@rneui/themed";
 import dayjs from "dayjs";
 
 export default function CalendarPage() {
   const [open, setOpen] = React.useState(false);
+  const [index, setIndex] = React.useState(0);
   const [list, setList] = React.useState([{}]);
   const [curDateString, setCurDateString] = React.useState<string>();
 
@@ -20,6 +21,7 @@ export default function CalendarPage() {
   const handlePressDay = (val: DateData) => {
     // console.log(JSON.stringify(val, null, 2));
     // console.log(new Date(val.dateString));
+    setCurDateString(val.dateString);
   };
 
   const handleMonthChange = (val: DateData) => {
@@ -28,16 +30,57 @@ export default function CalendarPage() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text h4>{dayjs(curDateString).format("MMM DD")}</Text>
-      </View>
       <View style={styles.headerDivision} />
       <Calendar
-        // items={arr}
-        // renderItem={AgendaItem}
-        // style={styles.info}
+        theme={{ dayTextColor: "#a6a6a6" }}
+        customHeader={(val: any) => (
+          <View>
+            <View style={styles.calendarHeader}>
+              <View>
+                <Text h4>{dayjs(curDateString).format("MMM DD")}</Text>
+                {/* TODO: 加个回到 today 的按钮 */}
+              </View>
+              <View
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  flexDirection: "row",
+                }}
+              >
+                <TouchableOpacity
+                  style={{ marginRight: 5 }}
+                  onPress={() => val.addMonth(-1)}
+                >
+                  <Icon name='chevron-left' style={styles.icon} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => val.addMonth(1)}>
+                  <Icon name='chevron-right' style={styles.icon} />
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                paddingTop: 10,
+                paddingBottom: 5,
+              }}
+            >
+              {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((item) => (
+                <Text
+                  key={item}
+                  style={{ flex: 1, textAlign: "center", color: "#1c242e" }}
+                >
+                  {item}
+                </Text>
+              ))}
+            </View>
+          </View>
+        )}
         initialDate={curDateString}
-        monthFormat={"MM-dd"}
+        markedDates={{ [`${curDateString}`]: { selected: true } }}
+        monthFormat={"MMM dd"}
         markingType={"dot"}
         enableSwipeMonths={true}
         hideExtraDays={true}
@@ -52,11 +95,20 @@ export default function CalendarPage() {
             padding: 5,
             paddingLeft: 10,
             fontWeight: "600",
+            fontSize: 20,
             color: "#4b4e6d",
           }}
         >
           Schedule
+          {/* TODO: 看这里可不可以加个切换成 Task 的效果 */}
         </Text>
+
+        <View style={{ width: 150 }}>
+          <Button type='clear' size='lg'>
+            Save
+            <Icon name='save' color='blue' />
+          </Button>
+        </View>
       </View>
 
       <SpeedDial
@@ -94,12 +146,23 @@ const styles = StyleSheet.create({
   },
   headerDivision: {
     backgroundColor: "white",
-    height: 20,
-    borderTopLeftRadius: 100,
-    borderTopRightRadius: 100,
+    height: 10,
+    // borderTopLeftRadius: 100,
+    // borderTopRightRadius: 100,
+  },
+  calendarHeader: {
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    paddingLeft: 15,
+    paddingRight: 15,
   },
   info: {
     backgroundColor: "white",
     minHeight: "100%",
+  },
+  icon: {
+    padding: 5,
+    fontSize: 20,
   },
 });
---
+++ components/ItemShow.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ItemShow = () => {
+  return <div>ItemShow</div>;
+};
+
+export default ItemShow;
