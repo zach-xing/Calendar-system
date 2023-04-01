b88ab7ef78a81830ffe0d721c8161bb28c752843
Zeekg <zeekg2001@gmail.com>
2023-04-01 19:53:08 +0800

    feat(web): add memo Page UI style

---
+++ web/src/pages/memo/components/MenuCard.tsx
@@ -0,0 +1,56 @@
+import { SmallDashOutlined } from "@ant-design/icons";
+import styled from "@emotion/styled";
+import { Button } from "antd";
+import React from "react";
+
+const StyleBox = styled.div`
+  width: 260px;
+  padding: 15px;
+  cursor: pointer;
+  border-bottom: 1px solid #cfcfcf;
+  &: hover {
+    background-color: #eee;
+  }
+  &:first-of-type {
+    border-top: 1px solid #cfcfcf;
+  }
+  .title {
+    width: 80%;
+    font-weight: bold;
+    white-space: nowrap; /* 禁止换行 */
+    overflow: hidden; /* 隐藏溢出部分 */
+    text-overflow: ellipsis;
+  }
+  .time {
+    font-size: 12px;
+  }
+`;
+
+interface IProps {
+  title: string;
+  lastModifiedTime: string;
+}
+
+/**
+ * 左边的导航卡片
+ */
+const MenuCard: React.FC<IProps> = (props) => {
+  return (
+    <StyleBox>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <p className='title'>{props.title}</p>
+        <Button icon={<SmallDashOutlined />} />
+      </div>
+      <div className='time'>最后修改时间: </div>
+      <div className='time'>{props.lastModifiedTime}</div>
+    </StyleBox>
+  );
+};
+
+export default MenuCard;
---
+++ web/src/pages/memo/index.tsx
@@ -0,0 +1,116 @@
+import styled from "@emotion/styled";
+import React, { useState } from "react";
+import ScrollBlock from "@/components/ScrollBlock";
+import MenuCard from "./components/MenuCard";
+import { Button, Form, Input, message } from "antd";
+import { IMemo } from "@/types";
+
+const ContainerStyleBox = styled.div`
+  display: flex;
+  margin-top: 10px;
+  .leftSider {
+    width: 260px;
+    background-color: white;
+    margin-right: 20px;
+  }
+  .rightContainer {
+    width: 100%;
+    background-color: white;
+    text-align: center;
+    h3 {
+      text-align: left;
+    }
+  }
+`;
+
+/**
+ * 备忘录
+ */
+const MemoPage = () => {
+  const [curMemo, setCurMemo] = useState<IMemo>();
+
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+    message.error("请填写完整");
+  };
+
+  return (
+    <>
+      <h1>备忘录</h1>
+      <Button type='primary'>创建</Button>
+      <ContainerStyleBox>
+        <div className='leftSider'>
+          <ScrollBlock height={"calc(100vh - 180px)"}>
+            <MenuCard
+              title={"asdasda撒旦水电费水电费水电费使得发送到"}
+              lastModifiedTime={"sdfsdf"}
+            />
+            <MenuCard title={"aaa"} lastModifiedTime={"sdfsd"} />
+            <MenuCard title={"sdfsdf"} lastModifiedTime={"sdfsdf"} />
+            <MenuCard title={"aaa"} lastModifiedTime={"sdfsd"} />
+            <MenuCard title={"sdfsdf"} lastModifiedTime={"sdfsdf"} />
+            <MenuCard title={"aaa"} lastModifiedTime={"sdfsd"} />
+            <MenuCard title={"sdfsdf"} lastModifiedTime={"sdfsdf"} />
+            <MenuCard title={"aaa"} lastModifiedTime={"sdfsd"} />
+            <MenuCard title={"sdfsdf"} lastModifiedTime={"sdfsdf"} />
+            <MenuCard title={"aaa"} lastModifiedTime={"sdfsd"} />
+          </ScrollBlock>
+        </div>
+
+        <div className='rightContainer'>
+          {!!curMemo ? (
+            <ScrollBlock height={"calc(100vh - 180px)"}>
+              <Form
+                name='basic'
+                style={{ maxWidth: 600, margin: "0 auto" }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete='off'
+              >
+                <h3 style={{ marginTop: 10 }}>标题</h3>
+                <Form.Item
+                  name='title'
+                  rules={[
+                    { required: true, message: "Please input your title!" },
+                  ]}
+                >
+                  <Input placeholder='标题' />
+                </Form.Item>
+
+                <h3>详情</h3>
+                <Form.Item name='text'>
+                  <Input.TextArea rows={10} placeholder='详情' />
+                </Form.Item>
+
+                <Form.Item>
+                  <Button type='primary' htmlType='submit'>
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+            </ScrollBlock>
+          ) : (
+            <div
+              style={{
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "#383838",
+              }}
+            >
+              <h2>请选择左边某个备忘录</h2>
+            </div>
+          )}
+        </div>
+      </ContainerStyleBox>
+    </>
+  );
+};
+
+export default MemoPage;
