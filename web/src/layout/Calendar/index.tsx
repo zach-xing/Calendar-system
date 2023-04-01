5487c783dbe2207d303dffeb5b82cae977bcc4e1
Zeekg <zeekg2001@gmail.com>
2023-04-01 21:07:41 +0800

    feat(web): add right calendar UI style

--- web/src/layout/Calendar/index.tsx
+++ web/src/layout/Calendar/index.tsx
@@ -1,22 +1,86 @@
-import React from "react";
-import { Calendar } from "antd";
+import React, { useMemo, useState } from "react";
+import { Badge, Button, Calendar, Dropdown, Space } from "antd";
 import type { CalendarMode } from "antd/es/calendar/generateCalendar";
 import type { Dayjs } from "dayjs";
-import { LayoutCalendar } from "./styled";
+import { CellStyleBox, DotBox, LayoutCalendar } from "./styled";
+import dayjs from "dayjs";
+import { DownOutlined } from "@ant-design/icons";
+
+const DATE_FORMET = "YYYY-MM-DD";
 
 /**
  * 日历部分
  */
 const CalendarLayout = () => {
+  const now = Date.now();
+  const [curSelectedDate, setCurSelectedDate] = useState(
+    dayjs(now).format(DATE_FORMET)
+  );
+
   const onPanelChange = (value: Dayjs, mode: CalendarMode) => {
-    console.log(value.format("YYYY-MM-DD"), mode);
+    console.log(value.format(DATE_FORMET), mode);
   };
+
+  const DateCellComp = useMemo(() => {
+    // eslint-disable-next-line react/display-name
+    return (date: Dayjs) => (
+      <>
+        <CellStyleBox
+          isCurMonth={dayjs(date).isSame(curSelectedDate, "month")}
+          isNow={dayjs(date).isSame(now, "day")}
+        >
+          <div
+            className={`${
+              dayjs(date).isSame(curSelectedDate, "day") ? "isSelected" : ""
+            }`}
+            style={{ padding: 5 }}
+          >
+            <div>{date.format("DD")}</div>
+          </div>
+        </CellStyleBox>
+        <DotBox isShow={true} />
+      </>
+    );
+  }, [curSelectedDate, now]);
 
   return (
     <LayoutCalendar>
-      <div style={{ width: 320, margin: "0 auto" }}>
-        <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+      <div style={{ width: 320, margin: "10px auto" }}>
+        <Calendar
+          fullscreen={false}
+          onPanelChange={onPanelChange}
+          onSelect={(date) => {
+            setCurSelectedDate(date.format(DATE_FORMET));
+          }}
+          dateFullCellRender={DateCellComp}
+        />
       </div>
+      <Dropdown
+        menu={{
+          items: [
+            {
+              label: <Button type='link'>创建日程</Button>,
+              key: "0",
+            },
+            {
+              label: <Button type='link'>创建任务</Button>,
+              key: "1",
+            },
+            {
+              label: <Button type='link'>创建备忘录</Button>,
+              key: "2",
+            },
+          ],
+        }}
+        trigger={["click"]}
+      >
+        <Button type='primary'>
+          <Space>
+            创建
+            <DownOutlined />
+          </Space>
+        </Button>
+      </Dropdown>
     </LayoutCalendar>
   );
 };
--- web/src/layout/Calendar/styled.ts
+++ web/src/layout/Calendar/styled.ts
@@ -3,4 +3,30 @@ import styled from "@emotion/styled";
 export const LayoutCalendar = styled.div`
   width: 360px;
   margin: 0 auto;
+  padding: 0 20px;
+`;
+
+/** 渲染单元格的 UI style */
+export const CellStyleBox = styled.div<{ isNow: boolean; isCurMonth: boolean }>`
+  color: ${(props) =>
+    props.isNow
+      ? "#1677ff"
+      : props.isCurMonth
+      ? "rgba(0, 0, 0, 0.88)"
+      : "rgba(0, 0, 0, 0.25)"};
+  border: ${(props) => (props.isNow ? "1px solid #1677ff" : "none")};
+  border-radius: 5px;
+  .isSelected {
+    border-radius: 5px;
+    color: white;
+    background-color: #1677ff;
+  }
+`;
+
+export const DotBox = styled.div<{ isShow: boolean }>`
+  width: 3px;
+  height: 3px;
+  margin: 0 auto;
+  border-radius: 50%;
+  background-color: ${(props) => (props.isShow ? "blue" : "white")};
 `;
