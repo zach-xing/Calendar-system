a8eec09323fc66d842d3cbfd0ddc15251cc67af0
Zeekg <zeekg2001@gmail.com>
2023-04-02 11:52:28 +0800

    chore(web): add react-query and axios lib

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,4 +1,13 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/home");
+  }, [router]);
+
   return (
     <>
       <main></main>
