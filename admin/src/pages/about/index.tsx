32c6c6eb62f54c375989467327751552a748921a
Zeekg <zeekg2001@gmail.com>
2023-04-03 21:38:41 +0800

    feat(admin): initial

---
+++ admin/src/pages/about/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+ * About
+ */
+const About: React.FC = () => {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+};
+
+export default About;
---
+++ admin/src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+ * 主页
+ */
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+};
+
+export default Home;
